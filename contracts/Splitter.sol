0d2f55dcfd9f8b77a9cb38f190544051520c74f4
Miguel De La Cruz <iscrusman@gmail.com>
2019-08-14 01:10:06 -0400

    Revision 2 Eth2
    Revision 2 Eth2

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -3,7 +3,6 @@ import './SafeMath.sol';
 import './Pausable.sol';
 
 contract Splitter is Pausable {
-    
     using SafeMath for uint; 
 
     mapping (address => uint) public balances;
@@ -16,7 +15,7 @@ contract Splitter is Pausable {
 
     //Split ETH from Alice
     function splitEth(address _bob, address _carol) public whenNotPaused payable returns (bool success){
-        require(_bob != address(0) , "Bob's accounts can not be empty.");
+        require(_bob != address(0),"Bob's accounts can not be empty.");
         require(_carol != address(0), "Carol's accounts can not be empty.");
 
         require(msg.value > 0,"Ether is required");
@@ -24,15 +23,16 @@ contract Splitter is Pausable {
 
 
         //Split
-        uint half = msg.value / 2;
+        uint half = msg.value.div(2);
 
         // add half Bob & Carol
         balances[_bob] = balances[_bob].add(half);
         balances[_carol] = balances[_carol].add(half);
 
-        //remainder for sender.
-        balances[msg.sender] = balances[_carol].add(msg.value % 2) ;
-
+        if (msg.value.mod(2) == 1) {
+            //remainder for sender.
+            balances[msg.sender] = balances[msg.sender].add(1);
+        }
         emit LogSplitter(msg.sender, _bob, _carol, msg.value);
         return true;
     }
@@ -42,12 +42,11 @@ contract Splitter is Pausable {
         // guards up front  
         require(balances[msg.sender] >= amount,"balance insufficient"); 
         // transfer
-        balances[msg.sender] -= amount;        
+        // balances[msg.sender] -= amount;     
+        balances[msg.sender] = balances[msg.sender].sub(amount);   
         emit LogWithdraw(msg.sender);
         msg.sender.transfer(amount);
         return true;
     }
-
-   
 
 }
